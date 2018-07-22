32fabc2e72e2275a966b9e12f291e0e9d9d93c35
pasawaya <psawaya@collegiateschool.org>
2018-07-21 20:30:35 -0400

    Debugging

--- models/MSESequenceLoss.py
+++ models/MSESequenceLoss.py
@@ -5,16 +5,19 @@ import torch.nn as nn
 class MSESequenceLoss(nn.Module):
     def __init__(self):
         super(MSESequenceLoss, self).__init__()
-        self.criterion = nn.MSELoss(size_average=False)
+        self.criterion = nn.MSELoss(size_average=True)
 
     def forward(self, inputs, targets):
+        return self.criterion
+
         batch_size = inputs.shape[0]
         T = inputs.shape[1]
         if targets.shape[1] != T:
             targets = targets.repeat(1, T, 1, 1, 1)
 
-        losses = []
-        for i in range(batch_size):
-            for t in range(T):
-                losses.append(self.criterion(inputs[i, t], targets[i, t]))
-        return sum(losses) / (batch_size * T)
+        return self.criterion(inputs, targets)
+        # losses = []
+        # for i in range(batch_size):
+        #     for t in range(T):
+        #         losses.append(self.criterion(inputs[i, t], targets[i, t]))
+        # return sum(losses) / (batch_size * T)
