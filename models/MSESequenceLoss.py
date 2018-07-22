6381f7b1ab6f582df92f19147a74c18319d2e573
pasawaya <psawaya@collegiateschool.org>
2018-07-21 20:47:54 -0400

    Loss working

--- models/MSESequenceLoss.py
+++ models/MSESequenceLoss.py
@@ -6,10 +6,8 @@ import torch
 class MSESequenceLoss(nn.Module):
     def __init__(self):
         super(MSESequenceLoss, self).__init__()
-        self.criterion = nn.MSELoss(size_average=True)
 
     def forward(self, inputs, targets):
-        batch_size = inputs.shape[0]
         T = inputs.shape[1]
         if targets.shape[1] != T:
             targets = targets.repeat(1, T, 1, 1, 1)
