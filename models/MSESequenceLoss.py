0895bfb5c7ff4198d421db8f969f6644731628cb
pasawaya <psawaya@collegiateschool.org>
2018-07-21 20:28:24 -0400

    Corrected loss function

--- models/MSESequenceLoss.py
+++ models/MSESequenceLoss.py
@@ -8,12 +8,13 @@ class MSESequenceLoss(nn.Module):
         self.criterion = nn.MSELoss(size_average=False)
 
     def forward(self, inputs, targets):
+        batch_size = inputs.shape[0]
         T = inputs.shape[1]
         if targets.shape[1] != T:
             targets = targets.repeat(1, T, 1, 1, 1)
 
         losses = []
-        for i in range(inputs.shape[0]):
+        for i in range(batch_size):
             for t in range(T):
                 losses.append(self.criterion(inputs[i, t], targets[i, t]))
-        return sum(losses)
+        return sum(losses) / (batch_size * T)
