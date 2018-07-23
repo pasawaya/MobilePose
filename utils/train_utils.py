4a8c7271fbd90ce2f82732f8c137a3237dff98de
pasawaya <psawaya@collegiateschool.org>
2018-07-23 10:11:57 -0400

    Works with conv layers with no bias

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -5,13 +5,16 @@ import torch.nn as nn
 def initialize_weights_normal(layer):
     if type(layer) == nn.Conv2d:
         nn.init.normal_(layer.weight, mean=0, std=0.01)
-        layer.bias.data.fill_(0.0)
+        if layer.bias is not None:
+            layer.bias.data.fill_(0.0)
 
 
 def initialize_weights_kaiming(layer):
     if type(layer) == nn.Conv2d:
         nn.init.kaiming_normal_(layer.weight, nonlinearity='relu')
-        layer.bias.data.fill_(0.0)
+
+        if layer.bias is not None:
+            layer.bias.data.fill_(0.0)
 
 
 def num_parameters(model):
