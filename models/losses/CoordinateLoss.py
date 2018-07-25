0243bc8f51fce37793c434a42ef3852ce438d922
pasawaya <psawaya@collegiateschool.org>
2018-07-25 11:12:48 -0400

    Added support for multiple stages

--- models/losses/CoordinateLoss.py
+++ models/losses/CoordinateLoss.py
@@ -1,14 +1,22 @@
 
 import torch.nn as nn
 import dsntnn
+import torch
 
 
 class CoordinateLoss(nn.Module):
     def __init__(self):
         super(CoordinateLoss, self).__init__()
 
-    def forward(self, coords, heatmaps, targets):
-        euc_loss = dsntnn.euclidean_losses(coords, targets)
-        reg_loss = dsntnn.js_reg_losses(heatmaps, targets, sigma_t=1.0)
-        loss = dsntnn.average_loss(euc_loss + reg_loss)
-        return loss
+    def forward(self, heatmaps, coords, targets):
+        n_stages = coords.shape[1]
+        if len(targets.shape) != len(coords.shape):
+            targets = torch.unsqueeze(targets, 1)
+            targets = targets.repeat(1, n_stages, 1, 1)
+
+        losses = []
+        for t in range(n_stages):
+            euc_loss = dsntnn.euclidean_losses(coords[:, t, :, :], targets[:, t, :, :])
+            reg_loss = dsntnn.js_reg_losses(heatmaps[:, t, :, :, :], targets[:, t, :, :], sigma_t=1.0)
+            losses.append(dsntnn.average_loss(euc_loss + reg_loss))
+        return sum(losses)
