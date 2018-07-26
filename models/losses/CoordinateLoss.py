d975ddcae4e632732f1d1cd9b1f56f37dc5a2335
pasawaya <psawaya@collegiateschool.org>
2018-07-26 13:19:57 -0400

    Debugging

--- models/losses/CoordinateLoss.py
+++ models/losses/CoordinateLoss.py
@@ -16,6 +16,8 @@ class CoordinateLoss(nn.Module):
 
         losses = []
         for t in range(n_stages):
+            print(type(coords))
+            print(type(targets))
             euc_loss = dsntnn.euclidean_losses(coords[:, t, :, :], targets[:, t, :, :])
             reg_loss = dsntnn.js_reg_losses(heatmaps[:, t, :, :, :], targets[:, t, :, :], sigma_t=1.0)
             losses.append(dsntnn.average_loss(euc_loss + reg_loss))
