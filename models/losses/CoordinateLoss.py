76ce9a5645a7ba4e848313470b809326c5f6c4f9
pasawaya <psawaya@collegiateschool.org>
2018-07-26 16:10:17 -0400

    corrected loss

--- models/losses/CoordinateLoss.py
+++ models/losses/CoordinateLoss.py
@@ -9,14 +9,16 @@ class CoordinateLoss(nn.Module):
         super(CoordinateLoss, self).__init__()
 
     def forward(self, heatmaps, coords, targets):
+        batch_size = coords.shape[0]
         n_stages = coords.shape[1]
+
         if len(targets.shape) != len(coords.shape):
             targets = torch.unsqueeze(targets, 1)
             targets = targets.repeat(1, n_stages, 1, 1)
 
         losses = []
-        for t in range(n_stages):
-            euc_loss = dsntnn.euclidean_losses(coords[:, t, :, :], targets[:, t, :, :])
-            reg_loss = dsntnn.js_reg_losses(heatmaps[:, t, :, :, :], targets[:, t, :, :], sigma_t=1.0)
+        for i in range(batch_size):
+            euc_loss = dsntnn.euclidean_losses(coords[i, :, :, :], targets[i, :, :, :])
+            reg_loss = dsntnn.js_reg_losses(heatmaps[i, :, :, :, :], targets[i, :, :, :], sigma_t=1.0)
             losses.append(dsntnn.average_loss(euc_loss + reg_loss))
-        return sum(losses)
+        return sum(losses) / batch_size
