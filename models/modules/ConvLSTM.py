cef2bd7babbdb55a4fc8612c915b199562344df1
pasawaya <psawaya@collegiateschool.org>
2018-07-30 14:09:34 -0400

    Reverted to own ConvLSTM

--- models/modules/ConvLSTM.py
+++ models/modules/ConvLSTM.py
@@ -4,121 +4,33 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-# Source: https://github.com/ndrplz/ConvLSTM_pytorch/blob/master/convlstm.py
-class ConvLSTMCell(nn.Module):
-    def __init__(self, input_dim, hidden_dim, kernel_size, device):
-        super(ConvLSTMCell, self).__init__()
-
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.device = device
-
-        self.kernel_size = kernel_size
-        self.padding = kernel_size[0] // 2, kernel_size[1] // 2
-
-        self.conv = nn.Conv2d(in_channels=self.input_dim + self.hidden_dim,
-                              out_channels=4 * self.hidden_dim,
-                              kernel_size=self.kernel_size,
-                              padding=self.padding,
-                              bias=True)
-
-    def forward(self, input_tensor, cur_state):
-        h_cur, c_cur = cur_state
-
-        combined = torch.cat([input_tensor, h_cur], dim=1)  # concatenate along channel axis
-
-        combined_conv = self.conv(combined)
-        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=1)
-        i = F.sigmoid(cc_i)
-        f = F.sigmoid(cc_f)
-        o = F.sigmoid(cc_o)
-        g = F.tanh(cc_g)
-
-        c_next = f.mul(c_cur) + i.mul(g)
-        h_next = o.mul(F.tanh(c_next))
-
-        return h_next, c_next
-
-    def init_hidden(self, x):
-        return torch.zeros_like(x).to(self.device), torch.zeros_like(x).to(self.device)
-
-
 class ConvLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, kernel_size, n_layers, device, return_all_layers=False):
+    def __init__(self, hidden_size, kernel_size, pad, bias, device):
         super(ConvLSTM, self).__init__()
 
-        self._check_kernel_size_consistency(kernel_size)
-
-        kernel_size = self._extend_for_multilayer(kernel_size, n_layers)
-        hidden_size = self._extend_for_multilayer(hidden_size, n_layers)
-        if not len(kernel_size) == len(hidden_size) == n_layers:
-            raise ValueError('Inconsistent list length.')
-
-        self.input_dim = input_size
-        self.hidden_dim = hidden_size
-        self.kernel_size = kernel_size
-        self.num_layers = n_layers
-        self.return_all_layers = return_all_layers
         self.device = device
 
-        cell_list = []
-        for i in range(0, self.num_layers):
-            cur_input_dim = self.input_dim if i == 0 else self.hidden_dim[i - 1]
+        self.i_x = nn.Conv2d(hidden_size, hidden_size, kernel_size, padding=pad, bias=bias)
+        self.o_x = nn.Conv2d(hidden_size, hidden_size, kernel_size, padding=pad, bias=bias)
+        self.g_x = nn.Conv2d(hidden_size, hidden_size, kernel_size, padding=pad, bias=bias)
+        self.f_x = nn.Conv2d(hidden_size, hidden_size, kernel_size, padding=pad, bias=bias)
 
-            cell_list.append(ConvLSTMCell(input_dim=cur_input_dim,
-                                          hidden_dim=self.hidden_dim[i],
-                                          kernel_size=self.kernel_size[i],
-                                          device=self.device))
+        self.i_h = nn.Conv2d(hidden_size, hidden_size, kernel_size, padding=pad, bias=bias)
+        self.o_h = nn.Conv2d(hidden_size, hidden_size, kernel_size, padding=pad, bias=bias)
+        self.g_h = nn.Conv2d(hidden_size, hidden_size, kernel_size, padding=pad, bias=bias)
+        self.f_h = nn.Conv2d(hidden_size, hidden_size, kernel_size, padding=pad, bias=bias)
 
-        self.cell_list = nn.ModuleList(cell_list)
+    def forward(self, x, h_prev, c_prev):
+        if h_prev is None:
+            h_prev = torch.zeros_like(x).to(self.device)
+        if c_prev is None:
+            c_prev = torch.zeros_like(x).to(self.device)
 
-    def forward(self, input_tensor, hidden_state=None):
-        if hidden_state is not None:
-            raise NotImplementedError()
-        else:
-            hidden_state = self._init_hidden(input_tensor)
+        i = F.sigmoid(self.i_x(x) + self.i_h(h_prev))
+        o = F.sigmoid(self.o_x(x) + self.o_h(h_prev))
+        g = F.sigmoid(self.g_x(x) + self.g_h(h_prev))
+        f = F.sigmoid(self.f_x(x) + self.f_h(h_prev))
 
-        layer_output_list = []
-        last_state_list = []
-
-        seq_len = input_tensor.size(1)
-        cur_layer_input = input_tensor
-
-        for layer_idx in range(self.num_layers):
-
-            h, c = hidden_state[layer_idx]
-            output_inner = []
-            for t in range(seq_len):
-                h, c = self.cell_list[layer_idx](input_tensor=cur_layer_input[:, t, :, :, :],
-                                                 cur_state=[h, c])
-                output_inner.append(h)
-
-            layer_output = torch.stack(output_inner, dim=1)
-            cur_layer_input = layer_output
-
-            layer_output_list.append(layer_output)
-            last_state_list.append([h, c])
-
-        if not self.return_all_layers:
-            layer_output_list = layer_output_list[-1:]
-            last_state_list = last_state_list[-1:]
-
-        return layer_output_list, last_state_list
-
-    def _init_hidden(self, x):
-        init_states = []
-        for i in range(self.num_layers):
-            init_states.append(self.cell_list[i].init_hidden(x))
-        return init_states
-
-    @staticmethod
-    def _check_kernel_size_consistency(kernel_size):
-        if not (isinstance(kernel_size, tuple) or
-                (isinstance(kernel_size, list) and all([isinstance(elem, tuple) for elem in kernel_size]))):
-            raise ValueError('`kernel_size` must be tuple or list of tuples')
-
-    @staticmethod
-    def _extend_for_multilayer(param, num_layers):
-        if not isinstance(param, list):
-            param = [param] * num_layers
-        return param
+        c = i.mul(g).add(f.mul(c_prev))
+        h = o.mul(F.tanh(c))
+        return h, c
