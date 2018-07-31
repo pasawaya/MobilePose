Fixed mean computation, initial commit of penn action mean

utils/dataset_utils.py
@@ -10,14 +10,14 @@ import cv2
 
 def compute_mean(dataset):
     mean, std = np.zeros(3, dtype=np.float), np.zeros(3, dtype=np.float)
-
     for i in range(len(dataset)):
-        print(i)
-        im, _, _, _ = dataset[i]
-        im = np.moveaxis(im, 2, 0)
-        im = im.reshape(im.shape[0], -1)
-        mean += np.mean(im, axis=1)
-        std += np.std(im, axis=1)
+        video, _, _, _ = dataset[i]
+        for t in range(len(video)):
+            im = video[t]
+            im = np.moveaxis(im, 2, 0)
+            im = im.reshape(im.shape[0], -1)
+            mean += np.mean(im, axis=1)
+            std += np.std(im, axis=1)
     mean = (mean / 255.) / float(len(dataset))
     std = (std / 255.) / float(len(dataset))
     return mean, std