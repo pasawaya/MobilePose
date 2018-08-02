Corrected mean computation and saving

utils/dataset_utils.py
@@ -19,7 +19,7 @@ def save_mean(dataset, device, path):
         std += video.std(2).sum(0)
     mean /= dataset.T * len(dataset)
     std /= dataset.T * len(dataset)
-    np.save(path, np.array([mean.numpy(), std.numpy()]))
+    np.save(path, np.array([mean.cpu().numpy(), std.cpu().numpy()]))
 
 
 def draw_skeleton(image, coordinates):