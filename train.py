Corrected mean computation and saving

train.py
@@ -115,22 +115,18 @@ def main(args):
     elif args.dataset == 'MPII':
         dataset = MPII
         transformer = ImageTransformer
-    elif args.dataset == 'lsp':
+    else:
         dataset = LSP
         transformer = ImageTransformer
-    else:
-        print('Invalid dataset chosen.')
-        sys.exit(0)
+
     root = os.path.join(args.data_dir, args.dataset)
-
-    mean_name = 'means.npy'
-    mean_path = os.path.join(root, mean_name)
+    mean_path = os.path.join(root, 'means.npy')
     if not os.path.isfile(mean_path):
-        mean, std = compute_mean(dataset(args.t, root=root, output_size=args.resolution, train=True),
-                                 device)
-        np.save(mean_path, np.array([mean, std]))
+        save_mean(dataset(args.t, root=root, output_size=args.resolution, train=True), device, mean_path)
 
-    mean, std = np.load(os.path.join(root, 'means.npy'))
+    mean, std = np.load(mean_path)
+    print(mean)
+    print(std)
     train_transformer = transformer(output_size=args.resolution, mean=mean, std=std)
     valid_transformer = transformer(output_size=args.resolution,
                                     p_scale=0.0, p_flip=0.0, p_rotate=0.0,

utils/dataset_utils.py
@@ -9,19 +9,19 @@ import torch
 import cv2
 
 
-def compute_mean(dataset, device):
+def save_mean(dataset, device, path):
     mean, std = torch.zeros(3).to(device), torch.zeros(3).to(device)
-    for i in range(len(dataset)):
+    for i in range(1):
         print(str(i) + ' / ' + str(len(dataset)))
         video, _, _, _, _ = dataset[i]
-        video = video.to(device)
-        for t in range(len(video)):
-            im = video[t]
-            mean += im.view(im.size(0), -1).mean(1)
-            std += im.view(im.size(0), -1).std(1)
-    mean /= 5 * len(dataset)
-    std /= 5 * len(dataset)
-    return mean, std
+        video = video.to(device).view(video.shape[0], video.shape[1], -1)
+        mean += video.mean(2).sum(0)
+        std += video.std(2).sum(0)
+    mean /= dataset.T * len(dataset)
+    std /= dataset.T * len(dataset)
+    print(mean)
+    print(std)
+    # np.save(path, np.array([mean.numpy(), std.numpy()]))
 
 
 def draw_skeleton(image, coordinates):